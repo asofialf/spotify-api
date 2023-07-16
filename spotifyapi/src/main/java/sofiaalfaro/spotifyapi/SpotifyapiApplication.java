f03dba023a5fc17b032a113a1bbc42a7dd3cc984
Sofia Alfaro <134465624+asofialf@users.noreply.github.com>
2023-07-16 14:37:59 -0500

    Add base file

---
+++ spotifyapi/src/main/java/sofiaalfaro/spotifyapi/SpotifyapiApplication.java
@@ -0,0 +1,13 @@
+package sofiaalfaro.spotifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyapiApplication.class, args);
+	}
+
+}
